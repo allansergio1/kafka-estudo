5a014c9b591967076fb9a4afa64e4d0d7636653d
Allan Ferreira <allan.ferreira@sefa.pa.gov.br>
2025-04-22 16:59:29 -0300

    feat: configuração do kafka na aplicação e no docker

---
+++ api1/src/main/java/br/com/api1/KafkaProducerConfig.java
@@ -0,0 +1,47 @@
+package br.com.api1;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.Map;
+
+@Configuration
+public class KafkaProducerConfig {
+
+    private final KafkaProperties kafkaProperties;
+
+    @Value("${topico.request.api}")
+    private String topico;
+
+    public KafkaProducerConfig(KafkaProperties kafkaProperties) {
+        this.kafkaProperties = kafkaProperties;
+    }
+
+    @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        Map<String, Object> properties = kafkaProperties
+                .buildProducerProperties();
+        return new DefaultKafkaProducerFactory<>(properties);
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
+    public NewTopic apiRequestTopicBuilder() {
+        return TopicBuilder
+                .name(topico)
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
+}
